6843452b7b4e77f34a1c638e81e0a9f3f314523b
dhimashf <muhammaddhimas1410@gmail.com>
2025-10-23 11:15:45 +0700

    fix cors

--- script.js
+++ script.js
@@ -22,7 +22,9 @@ document.addEventListener('DOMContentLoaded', () => {
         currentQuestionIndex: 0
     };
 
-    const API_BASE_URL = '/api/v1';
+    // Mengatur base URL API secara dinamis.
+    const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+    const API_BASE_URL = isLocalhost ? 'http://localhost:8000/api/v1' : '/api/v1';
     const token = localStorage.getItem('token');
 
     // Token check for protected pages
